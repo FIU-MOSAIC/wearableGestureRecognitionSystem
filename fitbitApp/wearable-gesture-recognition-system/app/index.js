cf9449700cc41fb577b5c14494aa6bd75dce9c22
orlandog0412 <orlandogonzz0412@gmail.com>
2024-07-02 20:09:09 -0400

    Fix

--- fitbitApp/wearable-gesture-recognition-system/app/index.js
+++ fitbitApp/wearable-gesture-recognition-system/app/index.js
@@ -2,90 +2,111 @@ import * as document from "document";
 import { Accelerometer } from "accelerometer";
 import { Gyroscope } from "gyroscope";
 import { OrientationSensor } from "orientation";
-import { messaging } from "messaging";
+import { display } from "display";
 
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
+const saveButton = document.getElementById("save");
+const retryButton = document.getElementById("retry");
 const centerText = document.getElementById("center-text");
-
 let hasHardwareComponents;
-let currentRun = {
-  timestamps: [],
-  accelX: [],
-  accelY: [],
-  accelZ: [],
-  gyroX: [],
-  gyroY: [],
-  gyroZ: [],
-  orientationScalar: [],
-  orientationI: [],
-  orientationJ: [],
-  orientationK: []
-};
 
-function recordRun() {
-  const freq = 1;
-  const batchNum = freq * 2;
+let currentRun;
 
-  const accel = new Accelerometer({ frequency: freq, batch: batchNum });
-  const gyro = new Gyroscope({ frequency: freq, batch: batchNum });
-  const orientation = new OrientationSensor({ frequency: freq, batch: batchNum });
-
-  accel.addEventListener("reading", () => {
-    for (let i = 0; i < accel.readings.timestamp.length; i++) {
-      currentRun.timestamps.push(accel.readings.timestamp[i]);
-      currentRun.accelX.push(accel.readings.x[i]);
-      currentRun.accelY.push(accel.readings.y[i]);
-      currentRun.accelZ.push(accel.readings.z[i]);
-    }
-  });
-
-  gyro.addEventListener("reading", () => {
-    for (let i = 0; i < gyro.readings.timestamp.length; i++) {
-      currentRun.gyroX.push(gyro.readings.x[i]);
-      currentRun.gyroY.push(gyro.readings.y[i]);
-      currentRun.gyroZ.push(gyro.readings.z[i]);
-    }
-  });
-
-  orientation.addEventListener("reading", () => {
-    for (let i = 0; i < orientation.readings.timestamp.length; i++) {
-      currentRun.orientationScalar.push(orientation.readings.scalar[i]);
-      currentRun.orientationI.push(orientation.readings.scalar[i]);
-      currentRun.orientationJ.push(orientation.readings.scalar[i]);
-      currentRun.orientationK.push(orientation.readings.scalar[i]);
-    }
-  });
-
-  accel.start();
-  gyro.start();
-  orientation.start();
-
-  stopButton.style.display = "inline";
-  stopButton.addEventListener("click", () => {
-    stopButton.style.display = "none";
-    accel.stop();
-    gyro.stop();
-    orientation.stop();
-    sendDataToCompanion();
-  });
-}
-
-function sendDataToCompanion() {
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send(currentRun);
-  }
-}
-
-function handleMessage(data) {
-  if (data && data.command === "start") {
-    recordRun();
-  }
-}
-
-messaging.peerSocket.onmessage = (evt) => {
-  handleMessage(evt.data);
-};
-
-startButton.style.display = "none";
 stopButton.style.display = "none";
+saveButton.style.display = "none";
+retryButton.style.display = "none";
+
+startButton.addEventListener("click", (evt) => {
+    startButton.style.display = "none";
+    hasHardwareComponents = Boolean(Accelerometer && Gyroscope && OrientationSensor);
+
+    if(!(hasHardwareComponents)){
+        centerText.text = "Device lacks required hardware to record exercises.";
+    } else {
+        recordRun();
+    }
+});
+
+function recordRun(){
+    stopButton.style.display = "inline";
+
+    const freq = 1;
+    const batchNum = freq * 2;
+
+    const accel = new Accelerometer({ frequency: freq, batch: batchNum });
+    const gyro = new Gyroscope({ frequency: freq, batch: batchNum });
+    const orientation = new OrientationSensor({ frequency: freq, batch: batchNum });
+
+    currentRun = {
+        timestamps: [],
+        accelX: [],
+        accelY: [],
+        accelZ: [],
+        gyroX: [],
+        gyroY: [],
+        gyroZ: [],
+        orientationScalar: [],
+        orientationI: [],
+        orientationJ: [],
+        orientationK: []
+    };
+
+    accel.addEventListener("reading", () => {
+        for(let i = 0; i < accel.readings.timestamp.length; i++){
+            currentRun.timestamps[i] = accel.readings.timestamp[i];
+            currentRun.accelX[i] = accel.readings.x[i];
+            currentRun.accelY[i] = accel.readings.y[i];
+            currentRun.accelZ[i] = accel.readings.z[i];
+        }
+    });
+
+    gyro.addEventListener("reading", () => {
+        for(let i = 0; i < gyro.readings.timestamp.length; i++){
+            currentRun.gyroX[i] = gyro.readings.x[i];
+            currentRun.gyroY[i] = gyro.readings.y[i];
+            currentRun.gyroZ[i] = gyro.readings.z[i];
+        }
+    });
+
+    orientation.addEventListener("reading", () => {
+        for(let i = 0; i < orientation.timestamp.length; i++){
+            currentRun.orientationScalar[i] = orientation.readings.scalar[i];
+            currentRun.orientationI[i] = orientation.readings.scalar[i];
+            currentRun.orientationJ[i] = orientation.readings.scalar[i];
+            currentRun.orientationK[i] = orientation.readings.scalar[i];
+        }
+    });
+
+    accel.start();
+    gyro.start();
+    orientation.start();
+
+    stopButton.addEventListener("click", (evt) => {
+        stopButton.style.display = "none";
+
+        accel.stop();
+        gyro.stop();
+        orientation.stop();
+
+        resultsScreen();
+    });
+}
+
+function resultsScreen(){
+    saveButton.style.display = "inline";
+    retryButton.style.display = "inline";
+
+    saveButton.addEventListener("click", (evt) => {
+        saveButton.style.display = "none";
+        retryButton.style.display = "none";
+        console.log(JSON.stringify(currentRun));
+        //upload results here
+    });
+
+    retryButton.addEventListener("click", (evt) => {
+        saveButton.style.display = "none";
+        retryButton.style.display = "none";
+        recordRun();
+    });
+}
