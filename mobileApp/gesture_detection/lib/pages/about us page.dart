de1158b89cf52a17f0c68dd226e5dfb83358e33f
wanSalas <70238165+wanSalas@users.noreply.github.com>
2024-07-16 12:35:47 -0400

    adding the description for the project in the about us page

--- mobileApp/gesture_detection/lib/pages/about us page.dart
+++ mobileApp/gesture_detection/lib/pages/about us page.dart
@@ -29,6 +29,32 @@ class AboutUsPage extends StatelessWidget {
             child: Container(
               color: Colors.white,
               width: double.infinity,
+              padding: EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    'In this project, students will develop a mobile/wearable app/service (for iOS, Android, and Fitbit devices) that provides hand motion instructions (either on the wearable or the phone) to a Fitbit user, which are then captured by the motion sensors of the wearable and sent back to the phone (and from there into a cloud service). '
+                        'The purpose is to measure things such as hand dexterity, range of motion, reflexes, hand-eye coordination, etc., for individuals with motor impairments (e.g., due to a stroke). '
+                        'In addition, a camera setup will be used to provide a reference for the hand/finger motions. Time permitting, the developed system will visualize the hand/fingers on the screen of the phone app or in a browser. '
+                        'Students will work with smartphone development tools (most likely Google\'s Flutter development platform), the Fitbit API and SDK, and various server/web tools.',
+                    style: TextStyle(fontSize: 16.0, color: Colors.black87),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
