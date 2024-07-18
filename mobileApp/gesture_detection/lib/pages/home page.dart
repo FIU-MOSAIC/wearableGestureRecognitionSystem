8a4b2e36cdaa0be8b3608e371e70c18912b49d51
orlandog0412 <orlandogonzz0412@gmail.com>
2024-07-18 16:46:17 -0400

    scroll view on home page

--- mobileApp/gesture_detection/lib/pages/home page.dart
+++ mobileApp/gesture_detection/lib/pages/home page.dart
@@ -88,100 +88,102 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            Center(
-              child: Text(
-                'Welcome ${user?.name ?? ''}',
+        child: SingleChildScrollView( // Added SingleChildScrollView here
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Center(
+                child: Text(
+                  'Welcome ${user?.name ?? ''}',
+                  style: GoogleFonts.lato(
+                    fontSize: 27.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Text(
+                "Recent Activities",
                 style: GoogleFonts.lato(
-                  fontSize: 27.0,
+                  fontSize: 23.0,
                   fontWeight: FontWeight.bold,
                   color: Colors.grey[600],
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            Text(
-              "Recent Activities",
-              style: GoogleFonts.lato(
-                fontSize: 23.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
+              const SizedBox(height: 10),
+              Text(
+                "Last Performed:",
+                style: GoogleFonts.lato(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Last Performed:",
-              style: GoogleFonts.lato(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Balance and Stability: ${userProvider.lastBalanceStabilityDate != null ? DateFormat('MMM dd, yyyy - h:mm a').format(userProvider.lastBalanceStabilityDate!) : 'No data available'}",
-                  style: GoogleFonts.lato(
+              const SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Balance and Stability: ${userProvider.lastBalanceStabilityDate != null ? DateFormat('MMM dd, yyyy - h:mm a').format(userProvider.lastBalanceStabilityDate!) : 'No data available'}",
+                    style: GoogleFonts.lato(
+                      fontSize: 15.0,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  Text("Range of Motion: [Date]", style: GoogleFonts.lato(
                     fontSize: 15.0,
                     color: Colors.grey[800],
+                  ),),
+                  Text(
+                    "Arm Mobility Test: ${userProvider.lastArmMobilityTestDate != null ? DateFormat('MMM dd, yyyy - h:mm a').format(userProvider.lastArmMobilityTestDate!) : 'No data available'}",
+                    style: GoogleFonts.lato(
+                      fontSize: 15.0,
+                      color: Colors.grey[800],
+                    ),
                   ),
-                ),
-                Text("Range of Motion: [Date]", style: GoogleFonts.lato(
-                  fontSize: 15.0,
-                  color: Colors.grey[800],
-                ),),
-                Text(
-                  "Arm Mobility Test: ${userProvider.lastArmMobilityTestDate != null ? DateFormat('MMM dd, yyyy - h:mm a').format(userProvider.lastArmMobilityTestDate!) : 'No data available'}",
-                  style: GoogleFonts.lato(
-                    fontSize: 15.0,
-                    color: Colors.grey[800],
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 15),
-            Divider(color: Colors.grey[400]),
-            const SizedBox(height: 15),
-            Text(
-              "Activities",
-              style: GoogleFonts.lato(
-                fontSize: 23.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey[600],
+                ],
               ),
-            ),
-            const SizedBox(height: 15.0),
-            ActivityTile(
-              title: 'Balance and Stability',
-              imagePath: 'lib/images/balance and stability.png',
-              width: 280,
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> const BalanceStabilityInstruction(title: 'Balance and Stability')));
-              },
-            ),
-            ActivityTile(
-              title: 'Range of Motion Test',
-              imagePath: 'lib/images/Range of Motion Icon.png',
-              width: 280,
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> const RangeOfMotionInstructionPage(title: 'Range of Motion Test')));
-              },
-            ),
-            ActivityTile(
-              title: 'Arm Mobility Test',
-              imagePath: 'lib/images/arm mobility.png',
-              width: 280,
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> const ArmMobilityInstructionPage(title: 'Arm Mobility')));
-              },
-            ),
-          ],
+              const SizedBox(height: 15),
+              Divider(color: Colors.grey[400]),
+              const SizedBox(height: 15),
+              Text(
+                "Activities",
+                style: GoogleFonts.lato(
+                  fontSize: 23.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[600],
+                ),
+              ),
+              const SizedBox(height: 15.0),
+              ActivityTile(
+                title: 'Balance and Stability',
+                imagePath: 'lib/images/balance and stability.png',
+                width: 280,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const BalanceStabilityInstruction(title: 'Balance and Stability')));
+                },
+              ),
+              ActivityTile(
+                title: 'Range of Motion Test',
+                imagePath: 'lib/images/Range of Motion Icon.png',
+                width: 280,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const RangeOfMotionInstructionPage(title: 'Range of Motion Test')));
+                },
+              ),
+              ActivityTile(
+                title: 'Arm Mobility Test',
+                imagePath: 'lib/images/arm mobility.png',
+                width: 280,
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const ArmMobilityInstructionPage(title: 'Arm Mobility')));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
